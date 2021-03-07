7b93d1dcdf754a5ff9fc0804be5141ab680a4c6c
Oskar619 <ozkar619@gmail.com>
2021-03-07 06:16:47 -0800

    Adding entity archetype

---
+++ src/Templates/EntityArchetype/CustomEntityArchetype.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Transforms
+
+namespace $rootnamespace$
+{
+    public static class $safeitemname$
+    {
+        public static ComponentType[] ComponentTypes = new ComponentType[]
+        {
+            new ComponentType(typeof(Translation)),
+            new ComponentType(typeof(Rotation))
+            // Add all required component types here..
+        };
+
+        public static EntityArchetype Create(EntityManager dstManager) 
+        {
+            return dstManager.CreateArchetype(ComponentTypes);
+        }
+    }
+}
